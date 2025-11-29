6c024c28c3897f4c5404303a938b18a350dc00ee
Nurrsssss <nursultantolegen238@gmail.com>
2025-11-29 05:17:58 +0500

    Add files via upload

---
+++ Superhero Randomizer App/Superhero Randomizer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Superhero Randomizer App
+//
+//  Created by Nursultan Tolegen on 29.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
